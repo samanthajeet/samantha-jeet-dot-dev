a693d37d07a396f57fccc7529341e37b774d175b
Samantha Jeet <samanthajeet@gmail.com>
2020-07-08 19:33:20 -0600

    google analytics

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import routes from './routes'
 import SocialMediaNav from './components/SocialMediaNav'
 import {AppContainer} from './reusableComponents/StyledComponents'
+import ReactGA from 'react-ga';
 import "./App.css";
+
+
+ReactGA.initialize('UA-44595509-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
